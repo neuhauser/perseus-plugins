73257add92ff307d6810458a7ca3f7e5109ef5b2
juergen <cox@biochem.mpg.de>
2013-01-05 18:16:48 +0100

    Comments added

--- PerseusApi/IActivity.cs
+++ PerseusApi/IActivity.cs
@@ -4,14 +4,20 @@ using BasicLib.Util;
 
 namespace PerseusApi{
 	/// <summary>
-	/// This interface is base from which all other activities are derived. 
+	/// This interface is the base from which all other activities are derived. 
 	/// It provides properties that are common to all activities. 
 	/// </summary>
 	public interface IActivity{
+		/// <summary>
+		/// This is the name that appears in the drop-down menu of Perseus to start this activity.
+		/// </summary>
 		string Name { get; }
 		float DisplayOrder { get; }
 		string HelpDescription { get; }
 		HelpType HelpDescriptionType { get; }
+		/// <summary>
+		/// If false is returned, the activity will not be available.
+		/// </summary>
 		bool IsActive { get; }
 		bool HasButton { get; }
 		Image ButtonImage { get; }
