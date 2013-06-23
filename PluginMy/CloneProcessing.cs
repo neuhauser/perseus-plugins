802d7e99d69464b95f6026b50913aab0f6d64c80
unknown <cox@PCMANN160.w2k.biochem.mpg.de>
2013-06-23 15:57:44 +0200

    .

--- PluginMy/CloneProcessing.cs
+++ PluginMy/CloneProcessing.cs
@@ -16,7 +16,7 @@ namespace PluginMy{
 		public HelpType[] HelpSupplTablesType { get { return new HelpType[0]; } }
 		public string[] HelpSupplTables { get { return new string[0]; } }
 		public int NumSupplTables { get { return 0; } }
-		public string Name { get { return "My clone"; } }
+		public string Name { get { return "Subtract constant"; } }
 		public string Heading { get { return "My plugins"; } }
 		public bool IsActive { get { return true; } }
 		public float DisplayOrder { get { return 100; } }
@@ -29,10 +29,19 @@ namespace PluginMy{
 		}
 
 		public void ProcessData(IMatrixData mdata, Parameters param, ref IMatrixData[] supplTables,
-			ref IDocumentData[] documents, ProcessInfo processInfo) {}
+			ref IDocumentData[] documents, ProcessInfo processInfo){
+			double shift = param.GetDoubleParam("shift").Value;
+			for (int i = 0; i < mdata.RowCount; i++){
+				for(int j = 0;j < mdata.ExpressionColumnCount; j++){
+					mdata[i, j] -= (float)shift;
+				}
+			}
+		}
 
 		public Parameters GetParameters(IMatrixData mdata, ref string errorString){
-			return new Parameters(new Parameter[]{});
+			return new Parameters(new Parameter[]{
+				new DoubleParam("shift", 0){Help = "This value is subtracted from each cell."} 
+			});
 		}
 	}
 }
---
+++ PluginMy/FilterDE.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using System.Drawing;
+using BasicLib.Param;
+using BasicLib.Util;
+using PerseusApi;
+using PerseusApi.Document;
+using PerseusApi.Matrix;
+
+namespace PluginMy{
+	public class FilterDE : IMatrixProcessing {
+		public bool HasButton { get { return false; } }
+		public Image ButtonImage { get { return null; } }
+		public string HelpDescription { get { return "Rows containing D or E in the specified columns are discarded."; } }
+		public string HelpOutput { get { return "Filtered matrix."; } }
+		public HelpType HelpDescriptionType { get { return HelpType.PlainText; } }
+		public HelpType HelpOutputType { get { return HelpType.PlainText; } }
+		public HelpType[] HelpSupplTablesType { get { return new HelpType[0]; } }
+		public string[] HelpSupplTables { get { return new string[0]; } }
+		public int NumSupplTables { get { return 0; } }
+		public string Name { get { return "Filter DE"; } }
+		public string Heading { get { return "My plugins"; } }
+		public bool IsActive { get { return true; } }
+		public float DisplayOrder { get { return 101; } }
+		public string[] HelpDocuments { get { return new string[0]; } }
+		public HelpType[] HelpDocumentTypes { get { return new HelpType[0]; } }
+		public int NumDocuments { get { return 0; } }
+
+		public int GetMaxThreads(Parameters parameters){
+			return 1;
+		}
+
+		public void ProcessData(IMatrixData mdata, Parameters param, ref IMatrixData[] supplTables,
+			ref IDocumentData[] documents, ProcessInfo processInfo){
+			int colIndex = param.GetSingleChoiceParam("Column with second last AA").Value;
+			string aas = param.GetStringParam("Amino acids").Value;
+			string[][] col = mdata.CategoryColumns[colIndex];
+			List<int> validRows = new List<int>();
+			for (int i = 0; i < mdata.RowCount; i++){
+				string[] x = col[i];
+				for (int j = 0; j < x.Length; j++){
+					if (x[j].Length != 1){
+						processInfo.ErrString = "Some of the entries in column " + mdata.CategoryColumnNames[colIndex] +
+							" do not contain amino acids";
+						return;
+					}
+				}
+				bool keep = JudgeIfKept(aas, x);
+				if (keep){
+					validRows.Add(i);
+				}
+			}
+			mdata.ExtractExpressionRows(validRows.ToArray());
+		}
+
+		private bool JudgeIfKept(string aas, string[] cat){
+			for (int i = 0; i < aas.Length; i++){
+				for (int j = 0; j < cat.Length; j++){
+					char x= aas[i];
+					char y = cat[j][0];
+					if (x == y){
+						return false;
+					}
+
+				}
+			}
+			return true;
+		}
+
+		public Parameters GetParameters(IMatrixData mdata, ref string errorString){
+			return new Parameters(new Parameter[]{
+				new SingleChoiceParam("Column with second last AA"){Values = mdata.CategoryColumnNames},
+ 				new StringParam("Amino acids", "DE") 
+			});
+		}
+	}
+}
